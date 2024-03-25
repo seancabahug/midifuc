158a6ea156e8b19c399101d748b3e9ad7d733761
Sean <25161923+seancabahug@users.noreply.github.com>
2024-03-25 09:24:18 -0700

    proper looping

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,13 +1,40 @@
 use crate::parser::{Instruction, Operation};
+use std::collections::HashMap;
 use std::{io, io::Read};
+
+fn find_loop_destinations(program: &Vec<Instruction>) -> HashMap<usize, usize> {
+    let mut loop_destinations = HashMap::new();
+
+    let mut loop_stack: Vec<usize> = vec![];
+    for i in 0..(program.len()) {
+        match program[i].operation {
+            Operation::BeginLoop => {
+                loop_stack.push(i);
+            }
+            Operation::EndLoop => {
+                let corresponding_begin_loop_index = loop_stack.pop().unwrap();
+
+                loop_destinations.insert(i, corresponding_begin_loop_index);
+                loop_destinations.insert(corresponding_begin_loop_index, i);
+            }
+            _ => {}
+        }
+    }
+
+    loop_destinations
+}
 
 pub fn interpret(program: Vec<Instruction>) {
     let mut memory_cells = [0; u16::MAX as usize];
     let mut memory_pointer: usize = 0;
     let mut instruction_pointer: usize = 0;
     let mut incrementing = true;
-    let mut return_address_stack: Vec<usize> = vec![];
     let mut increment_state_stack: Vec<bool> = vec![];
+
+    println!("INTERPRETING");
+
+    let loop_destinations = find_loop_destinations(&program);
+    dbg!(&loop_destinations);
 
     while instruction_pointer < program.len() {
         let instruction = &program[instruction_pointer];
@@ -24,15 +51,17 @@ pub fn interpret(program: Vec<Instruction>) {
                     incrementing = !incrementing;
                 }
                 Operation::BeginLoop => {
-                    return_address_stack.push(instruction_pointer);
-                    increment_state_stack.push(incrementing);
+                    if memory_cells[memory_pointer] == 0 {
+                        instruction_pointer = *loop_destinations.get(&instruction_pointer).unwrap();
+                    } else {
+                        increment_state_stack.push(incrementing);
+                    }
                 }
                 Operation::EndLoop => {
                     if memory_cells[memory_pointer] != 0 {
-                        instruction_pointer = return_address_stack.last().unwrap().clone();
+                        instruction_pointer = *loop_destinations.get(&instruction_pointer).unwrap();
                         incrementing = increment_state_stack.last().unwrap().clone();
                     } else {
-                        return_address_stack.pop();
                         increment_state_stack.pop();
                     }
                 }
@@ -40,8 +69,13 @@ pub fn interpret(program: Vec<Instruction>) {
                     print!("{}", char::from_u32(memory_cells[memory_pointer]).unwrap());
                 }
                 Operation::Input => {
-                    let value: u32 = io::stdin().bytes().next().unwrap().unwrap().into();
-                    memory_cells[memory_pointer] = value;
+                    let value = io::stdin().bytes().next();
+                    if let Some(Ok(v)) = value {
+                        memory_cells[memory_pointer] = v.into();
+                    } else {
+                        // something's wrong with input, either no more characters to read or something else. terminate here
+                        return;
+                    }
                 }
                 Operation::SpecialInstruction => {}
             }
